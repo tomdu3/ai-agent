bb72303ccf9743dcf7ebbd4d230d501e3f964906
Tomislav Dukez <tomdu3@ymail.com>
2025-05-12 17:03:44 +0100

    add basic llm setup

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+
+load_dotenv()
+
+
+llm = ChatOpenAI(model="gpt-40-mini")
+llm2 = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+
 def main():
     print("Hello from ai-agent!")
+    print("-" * 20)
+    response = llm2.invoke("What is the first thing you learn in Python?")
+    
+    print(response)
 
 
 if __name__ == "__main__":
